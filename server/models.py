8d631dcd27b4d7335cf9260f6c936fded9b75270
jeff <jeff.etale@student.moringaschool.com>
2023-09-25 21:27:22 +0300

    done lab

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+import pytz
 
 db = SQLAlchemy()
 
@@ -9,8 +11,8 @@ class Newsletter(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     body = db.Column(db.String)
-    published_at = db.Column(db.DateTime, server_default=db.func.now())
-    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+    published_at = db.Column(db.DateTime, default=datetime.now(pytz.timezone('Africa/Nairobi')))
+    edited_at = db.Column(db.DateTime, onupdate=datetime.now(pytz.timezone('Africa/Nairobi')))
 
     def __repr__(self):
         return f'<Newsletter {self.title}, published at {self.published_at}.>'
